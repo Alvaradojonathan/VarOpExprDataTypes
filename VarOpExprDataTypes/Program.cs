224971691de0750f277cf1476416f2e22fffc286
Your Name <you@example.com>
2017-06-27 15:09:26 -0400

    donation practice class

--- VarOpExprDataTypes/Program.cs
+++ VarOpExprDataTypes/Program.cs
@@ -10,6 +10,112 @@ namespace VarOpExprDataTypes
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your name?");
+            string donorName = Console.ReadLine();
+
+            Console.WriteLine("What is your address?");
+            string donorAddress = Console.ReadLine();
+
+            Console.WriteLine("What is your e-mail?");
+            string donorEmail = Console.ReadLine();
+
+            Console.WriteLine("What Fiscal year is it?");
+            string fiscalYear = Console.ReadLine();
+
+            Console.WriteLine("What is your donation amount?");
+            Console.WriteLine("How many $100 bills?");
+            double hundredBill = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many $50 bills?");
+            double fiftyBill = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many $20 bills?");
+            double twentyBill = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many $10 bills?");
+            double tenBill = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many $5 bills?");
+            double fiveBill = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many $1 bills?");
+            double oneBill = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many quarters?");
+            double quarters = double.Parse(Console.ReadLine());
+
+            double hundredValue = 100;
+            double fiftyValue = 50;
+            double twentyValue = 20;
+            double tenValue = 10;
+            double fiveValue = 5;
+            double oneValue = 1;
+            double quartersValue = 0.25;
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine("What is the name for your reservation?");
+            //string reservationName = Console.ReadLine();
+
+            //Console.WriteLine("How many people are in your party?");
+            //int numberOfPeople = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Reservation: " + reservationName);
+
+            //double amountEach = 13.53;
+            //double totalBill = (numberOfPeople*amountEach);
+            //Console.Write("Total Due: $" + totalBill);
+            //double totalDue = int.Parse(Console.ReadLine());
+
+            //double moneyAtStart = 29;
+            //double moneyAtEnd = 41;
+            //double moneyMade = moneyAtEnd - moneyAtStart;
+
+            //Console.WriteLine(moneyMade);
+
+
+
+
+            //int jessicaAge = 23;
+            //int samAge = 47;
+
+            //Console.WriteLine(jessicaAge < samAge);
+
+
+            //int age = 65;
+            //string why = "because I said so";
+            //char firstInitial = 'L';
+
+            ////Floating point types
+            //float floatNumber = 2.164403304897294849383961032f;
+            //Console.WriteLine(floatNumber);
+
+            //double doubleNumber = 2.164403304897294849383961032d;
+            //Console.WriteLine(doubleNumber);
+
+            //decimal decimalNumber = 2.164403304897294849383961032m;
+            //Console.WriteLine(decimalNumber);
+
+            //initializing values
+            //string name = "Mike";
+            //string birthMonth = "August";
+            //int 
+
+            //Console.WriteLine(name);
+
+            //name = "Daniel";
+            //Console.WriteLine(name);
+
+            //Console.WriteLine(birthMonth);
+
+
+
         }
     }
 }
